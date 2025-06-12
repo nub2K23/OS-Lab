7bceb57e7b4d4738bb41df3ebca45dd85ab8617c
Rahul Arbind <150941574+nub2K23@users.noreply.github.com>
2025-06-12 23:15:27 +0530

    Create exp1.c

---
+++ exp1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main() {
+    int pid, ret, status;
+    pid = fork();
+    if(pid == 0) {
+        execvp("./", "parent", NULL);
+    } else if (pid > 0) {
+        ret = waitpid(pid, &status, 0);
+        printf("\n I am in parent \n");
+        if (ret == -1) {
+            printf("\nError in wait ID \n");
+        } else if (ret == pid) {
+            printf("I am waiting in parent \n");
+        } else {
+            printf("Error in fork command \n");
+        }
+    }
+
+    return 0;
+}
