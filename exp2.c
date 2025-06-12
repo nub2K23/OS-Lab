d55d3da916979a3e237fc33a1b08b2cd2b47da94
Rahul Arbind <150941574+nub2K23@users.noreply.github.com>
2025-06-12 23:48:20 +0530

    exp2.c v0.1

--- exp2.c
+++ exp2.c
@@ -4,10 +4,13 @@
 int main() {
     printf("\nKernel version: ");
     system("cat /proc/sys/kernel/osrelease");
+
     printf("\nCPU specifications: \n");
     system("cat /proc/cpuinfo");
+
     printf("\nSystem uptime: ");
-    system("cat /proc/uptime\n");
-    
+    system("cat /proc/uptime");
+    printf("\n");  // add newline for neat output
+
     return 0;
 }
