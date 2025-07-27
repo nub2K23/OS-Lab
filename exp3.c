a39e59598370883fa701c3a92d74542a05cb9eab
Rahul Arbind <150941574+nub2K23@users.noreply.github.com>
2025-07-27 18:03:29 +0530

    Update exp3.c

--- exp3.c
+++ exp3.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    printf("\nConfigured Memory: ");
+int main()
+{
+    printf("\n The Configured  Memory is:\n");
     system("cat /proc/meminfo | awk 'NR==1{print $2}'\n");
-
-    printf("\nFree memory availabe: ");
-    system("cat /proc/meminfo | awk 'NR==2{print $2}'\n");
-
-    printf("\nMemory used: ");
-    system("cat /proc/meminfo | awk '{if(NR==1) a =$2;if(NR==2) b=$2} END{print a-b}'\n");
-    
+    printf("\n Amount of Free Memory:\n");
+    system("cat /proc/meminfo | awk 'NR==2{print $2}'\n ");
+    printf("\n Amount of used Memory:\n");
+    system("cat /proc/meminfo | awk '{if(NR==1) a=$2;if(NR==2) b=$2} END{print ab}'\n");
 }
