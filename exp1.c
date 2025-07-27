42cec9bf961a89501a97636af47ec6f6fa40ff2a
Rahul Arbind <150941574+nub2K23@users.noreply.github.com>
2025-07-27 18:02:51 +0530

    Update exp1.c

--- exp1.c
+++ exp1.c
@@ -3,23 +3,30 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-int main() {
+int main()
+{
     int pid, ret, status;
     pid = fork();
 
-    if (pid == 0) {
-        execl("./", "parent", NULL);
-    } else if (pid > 0) {
-        ret = waitpid(pid, &status, 0);
-        printf("\n I am in parent \n");
-        if (ret == -1) {
-            printf("\nError in wait ID \n");
-        } else if (ret == pid) {
-            printf("I am waiting in parent \n");
-        } else {
-            printf("Error in fork command \n");
-        }
+    if (pid == 0)
+    {
+        // exevp("./child",NULL); // same program different code
+        execl("./", "parent", NULL); // same program same code
     }
 
+    else if (pid > 0)
+    {
+        ret = waitpid(pid, &status, 0);
+        printf("\n I am in parent \n");
+        if (ret == -1)
+            printf("\nError in wait ID \n");
+        else if (ret == pid)
+            printf("I am waiting in parent \n");
+    }
+
+    else
+    {
+        printf("Error in fork command \n");
+    }
     return 0;
 }
